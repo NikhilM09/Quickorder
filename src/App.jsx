98a4d0324270608ad81ec17b35ea7e16853a7fb5
NikhilM09 <mahadiknikhil28@gmail.com>
2025-06-16 20:50:49 +0530

    header component added

--- src/App.jsx
+++ src/App.jsx
@@ -3,91 +3,19 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const styleObj = {
-    color: "black",
-    backgroundColor: "white",
-    border: "2px solid yellow",
-    borderRadius: "5px",
-  };
-
-  const element = (
-    <>
-      <h1 className="text-red-400 bg-amber-400 text-7xl ">this is react element</h1>
-      <h2>This is second react element</h2>
-    </>
-  );
-
-  const Component = () =>{
-    return(
-      <>
-        This is my first function based component
-      </>
-    )
-  }
-
-    // const Component = () =>
-    // (
-    //   <>
-    //     This is my first function based component
-    //     <div>Second component</div>
-    //   </>
-    // )
-
-    
   
-
-  // function Component(){
-  //   return(
-  //     <>
-  //       This is my first function based component
-  //     </>
-  //   )
-  // }
-
-  // function Component(){
-  //   return(
-  //     <>
-  //       This is my first function based component
-  //     </>
-  //   )
-  // }
 
   return (
     <>
-      <div>
-        {element}
-        {/* <Component></Component> */}
-        <Component/>
-        {/* {Component()} */}
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div style={{ color: "black" }} className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to see HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header/>
     </>
   );
 }
 
-export function App2() {
-  return <div>This is second component</div>;
-}
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div className="flex justify-between p-5 bg-amber-300">
+      <div>Logo</div>
+      <div className="flex gap-7">
+        <div>Home🏡</div>
+        <div>About us</div>
+        <div>Contact us📞</div>
+        <div>Cart🛒</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
